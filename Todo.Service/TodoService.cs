15e666439920c03623d948d8aa9c69819616de0e
Borillo-Miguel-BSCS32E1 <miguelborillo2002@gmail.com>
2024-05-04 17:21:03 +0800

    Update TodoService.cs

--- Todo.Service/TodoService.cs
+++ Todo.Service/TodoService.cs
@@ -7,59 +7,60 @@ namespace Todo.Service;
 public class TodoService(ITodoRepository repository) : ITodoService
 {
     public async Task<bool> Delete(int id)
+{
+    var todo = await repository.GetByIdAsync(id);
+    todo.IsActive = false;
+    repository.Update(todo);
+    await repository.SaveChangesAsync(default);
+    return true;
+}
+
+public async Task<bool> MarkAsDone(int id)
+{
+    var todo = await repository.GetByIdAsync(id);
+    todo.Status = Status.Done;
+    repository.Update(todo);
+    await repository.SaveChangesAsync(default);
+
+    return true;
+}
+
+public IEnumerable<TodoModel> GetAll()
+{
+    return repository.GetAll().Select(c => new TodoModel()
     {
-        var todo = await repository.GetByIdAsync(id);
-        todo.IsActive = false;
-        repository.Update(todo);
-        await repository.SaveChangesAsync(default);
-        return true;
-    }
+        Category = c.Category,
+        IsActive = c.IsActive,
+        Description = c.Description,
+        DueDate = c.DueDate,
+        Status = c.Status,
+        Id = c.Id
+    }).ToArray();
+}
 
-    public async Task<bool> MarkAsDone(int id)
+public async Task<int> Add(AddTodoModel model)
+{
+    repository.Add(new ToDo()
     {
-        var todo = await repository.GetByIdAsync(id);
-        todo.Status = Status.Done;
-        repository.Update(todo);
-        await repository.SaveChangesAsync(default);
+        Category = model.Category,
+        Description = model.Description,
+        DueDate = model.DueDate,
+        IsActive = true,
+        Status = Status.New,
+    });
 
-        return true;
-    }
+    return await repository.SaveChangesAsync();
 
-    public IEnumerable<TodoModel> GetAll()
-    {
-        return repository.GetAll().Select(c => new TodoModel()
-        {
-            Category = c.Category,
-            IsActive = c.IsActive,
-            Description = c.Description,
-            DueDate = c.DueDate,
-            Status = c.Status,
-            Id = c.Id
-        }).ToArray();
-    }
+}
 
-    public async Task<int> Add(AddTodoModel model)
-    {
-        repository.Add(new ToDo()
-        {
-            Category = model.Category,
-            Description = model.Description,
-            DueDate = model.DueDate,
-            IsActive = true,
-            Status = Status.New,
-        });
+public async Task<int> UpdateDescription(int id, string description)
+{
+    var todo = await repository.GetByIdAsync(id);
+    todo.Description = description;
 
-        return 0;
-    }
+    repository.Update(todo);
+    await repository.SaveChangesAsync(default);
 
-    public async Task<int> UpdateDescription(int id, string description)
-    {
-        var todo = await repository.GetByIdAsync(id);
-        todo.Description = description;
-
-        repository.Update(todo);
-        await repository.SaveChangesAsync(default);
-
-        return 0;
-    }
+    return 0;
+}
 }

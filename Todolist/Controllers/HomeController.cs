8c75de27d7292d67f66b15aae94fad833f55a43f
Borillo-Miguel-BSCS32E1 <miguelborillo2002@gmail.com>
2024-05-04 17:21:57 +0800

    Update HomeController.cs

--- Todolist/Controllers/HomeController.cs
+++ Todolist/Controllers/HomeController.cs
@@ -1,130 +1,125 @@
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Todo.Service;
 using TodoList.Domain;
 using Todolist.Models;
 using TodoList.Domain.Interface;
+using System.Security.Cryptography.X509Certificates;
+using static Todolist.Controllers.HomeController;
 
 
 namespace Todolist.Controllers
 {
     public class HomeController(ITodoService service) : Controller
     {
-        
-        public IActionResult Index(string id)
-        {
-            var temp = service.GetAll();
-            var model = new TodoModel();
-            model.Category = model with { Category = Category.Adventure };
-            
-            //var filters = new Filters(id);
-            //ViewBag.Filters = filters;
-            ViewBag.Categories = Enum.GetValues(typeof(Category));
-            ViewBag.Statuses =  Enum.GetValues(typeof(Status));
-            //ViewBag.DueFilters = Filters.DueFilterValues;
-            var query = service.GetAll();
-            //IQueryable<ToDo> query = context.ToDoS
-            //    .Include(t => t.Category)
-            //    .Include(t => t.Status);
+        private readonly dynamic _categories = Enum.GetValues(typeof(Category)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
+    private readonly dynamic _stasuses = Enum.GetValues(typeof(Status)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
+    public IActionResult Index(string id)
+    {
+        var temp = service.GetAll();
 
-            //if (filters.HasCategory)
-            //{
-            //    query = query.Where(t => t.CategoryId == filters.CategoryId);
-            //}
+        var filters = new Filters(id);
+        ViewBag.Filters = filters;
+        ViewBag.Categories = _categories;
+        ViewBag.Statuses = _stasuses;
+        ViewBag.DueFilters = Filters.DueFilterValues;
+        var query = service.GetAll();
+        //IQueryable<ToDo> query = context.ToDoS
+        //    .Include(t => t.Category)
+        //    .Include(t => t.Status);
 
-            //if (filters.HasStatus)
-            //{
-            //    query = query.Where(t => t.StatusId == filters.StatusId);
-            //}
+        //if (filters.HasCategory)
+        //{
+        //    query = query.Where(t => t.CategoryId == filters.CategoryId);
+        //}
 
-            //if (filters.HasDue)
-            //{
-            //    var today = DateTime.Today;
-            //    if (filters.IsPast)
-            //    {
-            //        query = query.Where(t => t.DueDate < today);
-            //    }
-            //    else if (filters.IsFuture)
-            //    {
-            //        query = query.Where(t => t.DueDate > today);
+        //if (filters.HasStatus)
+        //{
+        //    query = query.Where(t => t.StatusId == filters.StatusId);
+        //}
 
-            //    }
-            //    else if (filters.IsToday)
-            //    {
-            //        query = query.Where(t => t.DueDate == today);
-            //    }
-            //}
-            //var tasks = query.OrderBy(t => t.DueDate).ToList();
+        //if (filters.HasDue)
+        //{
+        //    var today = DateTime.Today;
+        //    if (filters.IsPast)
+        //    {
+        //        query = query.Where(t => t.DueDate < today);
+        //    }
+        //    else if (filters.IsFuture)
+        //    {
+        //        query = query.Where(t => t.DueDate > today);
 
-            //return View(tasks);
+        //    }
+        //    else if (filters.IsToday)
+        //    {
+        //        query = query.Where(t => t.DueDate == today);
+        //    }
+        //}
+        //var tasks = query.OrderBy(t => t.DueDate).ToList();
 
-            return View();
-        }
+        //return View(tasks);
 
-        [HttpGet]
-        public IActionResult Add()
-        {
-            ViewBag.Categories = context.Categories.ToList();
-            ViewBag.Statuses = context.Statuses.ToList();
-            var task = new ToDo { StatusId = "open" };
-            return View(task);
-        }
-
-        [HttpPost]
-        public IActionResult Add(AddTodoModel task)
-        {
-            if (ModelState.IsValid)
-            {
-                context.ToDoS.Add(task);
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                ViewBag.Categories = context.Categories.ToList();
-                ViewBag.Statuses = context.Statuses.ToList();
-                return View(task);
-            }
-        }
-
-        [HttpPost]
-        public IActionResult Filter(string[] filter)
-        {
-            string id = string.Join("-", filter);
-            return RedirectToAction("Index", new { ID = id });
-        }
-        [HttpPost]
-        public IActionResult MarkComplete([FromRoute] string id, ToDo selected)
-        {
-            selected = context.ToDoS.Find(selected.Id);
-
-            if (selected != null)
-            {
-                selected.StatusId = "closed";
-                context.SaveChanges();
-            }
-            return RedirectToAction("Index", new { ID = id });
-        }
-        
-        [HttpPost]
-        public async Task<IActionResult> MarkComplete([FromRoute] string id, ToDo selected)
-        {
-            var markedAsDone = await service.MarkAsDone(int.Parse(id));
-            
-            return RedirectToAction("Index", new { ID = id });
-        }
-        [HttpPost]
-        public IActionResult DeleteComplete(string id)
-        {
-            var toDelete = context.ToDoS.Where(t => t.StatusId == "closed").ToList();
-
-            foreach (var task in toDelete)
-            {
-                context.ToDoS.Remove(task);
-            }
-            context.SaveChanges();
-
-            return RedirectToAction("Index", new { ID = id });
-        }
+        return View(temp);
     }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        ViewBag.Categories = _categories;
+        ViewBag.Statuses = _stasuses;
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(AddTodoModel task)
+    {
+        if (ModelState.IsValid)
+        {
+            service.Add(task);
+
+            return RedirectToAction("Index");
+        }
+
+        ViewBag.Categories = _categories();
+        ViewBag.Statuses = _stasuses();
+        return View(task);
+
+    }
+
+    //[HttpPost]
+    //public IActionResult Filter(string[] filter)
+    //{
+    //    string id = string.Join("-", filter);
+    //    return RedirectToAction("Index", new { ID = id });
+    //}
+    //[HttpPost]
+    //public IActionResult MarkComplete([FromRoute] string id, ToDo selected)
+    //{
+    //    selected = context.ToDoS.Find(selected.Id);
+
+    //    if (selected != null)
+    //    {
+    //        selected.StatusId = "closed";
+    //        context.SaveChanges();
+    //    }
+    //    return RedirectToAction("Index", new { ID = id });
+    //}
+
+    [HttpPost]
+    public async Task<IActionResult> MarkComplete([FromRoute] int id, ToDo selected)
+    {
+        var markedAsDone = await service.MarkAsDone((id));
+
+        return RedirectToAction("Index", new { ID = id });
+    }
+    [HttpPost]
+    public IActionResult DeleteComplete(int id)
+    {
+        service.Delete(id);
+
+        return RedirectToAction("Index", new { ID = id });
+    }
+}
 }
